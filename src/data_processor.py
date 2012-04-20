1efe898b5b39c56081af229c3ebd051afa6a826a
Valeriy Savchenko <sinmipt@gmail.com>
2012-04-21 01:59:22 +0400

    WIP. Comments added

--- src/data_processor.py
+++ src/data_processor.py
@@ -4,6 +4,8 @@ import pickle, os
 users = {}
 users_marks = []
 
+#all data in downloaded files is represented
+#in a table HTML format, so we need to parse it
 def parse_data(number):
     xlsname = form_xls_name(number)
     xlsfile = open(xlsname)
@@ -14,16 +16,25 @@ def parse_data(number):
     rows = table.findAll('tr')
     marks = []
     for tr in rows:
+        #adding new row
         marks.append([])
         cols = tr.findAll('td')
         for td in cols:
+            #avoiding extra tags in movie titles
             text = td.contents[0] if td.contents else ''
             text = text.contents[0] if hasattr(text,"contents") \
                                     and text.contents else text
             text = str(text)
             marks[-1].append(text)
-        
+    
+    #deleting table's head    
     marks=marks[1:]
+    #this magic generates the list that will be smth like that
+    #("Movie Title(on russian or in original) YEAR", mark (or 0 if u just watched it)
+    # 0 - russian title
+    # 1 - native title
+    # 2 - year
+    # 8 - mark (could be '-')
     marks = [((row[0] if row[0]!='' else row[1]) + " " + row[2] \
               , int(row[8]) if row[8]!='-' else 0) for row in marks]
     return marks
@@ -32,18 +43,20 @@ def parse_data(number):
 def form_xls_name(number):
     return "../data/"+str(number)+".xls"
 
+#dump data that represents users we've already process
 def dump_users_marks():
     global users_marks
     pickle_file = open("../data/marks/users_proceeded",'w')
     pickle.dump(users_marks, pickle_file)
     pickle_file.close()
 
+#load all users list
 def recover_users():
     global users
     pickle_file = open("../data/users",'r')
     users = pickle.load(pickle_file)
     pickle_file.close()
-    
+
 def recover_users_marks():
     global users_marks
     if not os.path.exists("../data/marks/users_proceeded"): return
@@ -51,6 +64,7 @@ def recover_users_marks():
     users_marks = pickle.load(pickle_file)
     pickle_file.close()  
 
+#store users marks data
 def dump_users_data(number):
     pickle_file = open("../data/marks/"+str(number),'w')
     pickle.dump(parse_data(number), pickle_file)
@@ -61,10 +75,10 @@ def process_users():
     recover_users_marks()
     a = set(users)
     b = set(users_marks)
+    #dealing with not processed users
     c = a - b
     print "Start"
     for number in c:
-#        if number in users_marks.keys(): continue
         try:
             dump_users_data(number)
             users_marks.append(number)
