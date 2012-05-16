3b14984a84d63c6aae953d75ea36c12a4d56fd4a
Stas Fomin <stas-fomin@yandex.ru>
2012-05-16 14:15:40 +0400

    Codereview from Stas Fomin

--- src/MAX_CUT.py
+++ src/MAX_CUT.py
@@ -1,3 +1,4 @@
+﻿# -*- coding: utf-8 -*-
 import random
 users = [[1, 2, 9, 1, 8, 3, 4],
 		 [2, 1,10, 3, 7, 2, 1],
@@ -6,32 +7,48 @@ users = [[1, 2, 9, 1, 8, 3, 4],
 		 [8, 7, 4, 6, 1, 1, 9],
 		 [3, 2, 3, 1, 1, 1, 2],
 		 [4, 1, 1, 7, 9, 2, 1]]
+
+# Нужно использовать в numpy http://numpy.scipy.org/
+# Есть нативная сериализация
+# http://thsant.blogspot.com/2007/11/saving-numpy-arrays-which-is-fastest.html         
+         
 #number of users
 NUMBER = 7
+# Глобальные переменные SUCKS
 
-#find weight of cut
+# Посмотрите http://www.python.org/dev/peps/pep-0008/
+
 def Cut_weight():
+	"""
+	find weight of cut
+	"""
 	weight = 0
-	for i in range(NUMBER):
-		for j in range(i):
+	for i in xrange(NUMBER):
+		for j in xrange(i):
 			if users[i][i] != users[j][j]:
 				weight = weight + users[i][j]
 	return weight
+
+   
 #find difference between internal and external edges 
-def Find_sum(index):
-	i = 0
+def irrelevant_metric(index):
+	"""
+	index — индекс пользователя
+	
+	Возращает разность суммы antisimilarity с однокластерниками 
+	минус антисимилярити с врагами.
+	"""
 	sum = 0
-	for i in range(NUMBER):
-		sum = sum + users[index][i]*users[i][i]*users[index][index]
-		i = i + 1
+	for i in xrange(NUMBER):
+		sum += users[index][i] * users[i][i] * users[index][index]
 	sum = sum - users[index][index]
 	return sum
 
 #simple random method
 def Random_CUT():
-	for i in range(NUMBER):
-		r = random.random()*2
-		if r<1:
+	for i in xrange(NUMBER):
+		r = random.random() * 2
+		if r < 1:
 			users[i][i] = 1
 		else:
 			users[i][i] = -1
@@ -41,14 +58,14 @@ def Max_CUT():
 	while 1:
 		max_sum = 0
 		index_to_change = 0
-		for i in range(NUMBER):
-			sum = Find_sum(i)
+		for i in xrange(NUMBER):
+			sum = irrelevant_metric(i)
 			if sum > max_sum:
 				max_sum = sum
 				index_to_change = i
 		if max_sum == 0:
 			break
-		users[index_to_change][index_to_change] = (-1)*users[index_to_change][index_to_change]
+		users[index_to_change][index_to_change] *= -1
 	
 Max_CUT()
 
